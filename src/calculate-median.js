bcd18766f2a55fbee6714ec9d3b8bb1c5104e162
code <1165444511@qq.com>
2016-05-22 12:18:53 +0800

    .

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,30 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+    var objectArray = buildObjectArray(arr);
+    return buildMedian(objectArray, arr);
+}
+
+function buildObjectArray(arr) {
+    var objectArray;
+    var count = 0;
+    var sum = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        var array = arr[i];
+        if (array % 2 === 0) {
+            sum += i;
+            count++;
+        }
+    }
+    objectArray = {sum: sum, count: count};
+
+    return objectArray;
+}
+
+function buildMedian(objectArray, arr) {
+    var position = objectArray.sum / objectArray.count;
+    for (var i = 0; i < position; i++);
+
+    return arr[i];
 }
 
 module.exports = calculate_median;
